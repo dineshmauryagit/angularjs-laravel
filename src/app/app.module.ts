9bd7f5bd5bcc922a2fb68a163af4a947ecbaf170
Dinesh Maurya <dineshmaurya@hotmail.com>
2017-10-01 22:51:18 +0530

    angular js app with laravel api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
+import { routing } from "./app.routing";
+import { QuoteService } from "./quote.service";
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { AuthService } from './auth.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuotesComponent,
+    NewQuoteComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [QuoteService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/quote.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from "@angular/core";
+import { Http, Response, Headers } from "@angular/http";
+import 'rxjs/Rx';
+import { Observable } from "rxjs";
+import { AuthService } from './auth.service';
+
+
+
+@Injectable()
+export class QuoteService {
+	constructor(private http: Http, private authService: AuthService){
+
+	}
+
+	addQuote(content: string) {
+		const token =this.authService.getToken();
+		const body = JSON.stringify({content: content});
+		const headers = new Headers({ 'Content-Type': 'application/json' });
+		return this.http.post('http://localhost/LAV/public/api/quote?token='+token, body, {headers: headers})
+	}
+
+	getQuotes(): Observable<any> {
+		return this.http.get('http://localhost/LAV/public/api/quote')
+		.map(
+			(response: Response) => {
+				return response.json().quotes;
+			}
+			);
+	}
+
+	updateQuote(id: number, newContent: string) {
+		const token =this.authService.getToken();
+		const body = JSON.stringify({content: newContent});
+		const headers = new Headers({'Content-Type': 'application/json'});
+		return this.http.put('http://localhost/LAV/public/api/quote/' + id+'?token='+token, body, {headers:headers})
+		.map(
+			(response: Response) => response.json()
+			);
+	}
+
+	deleteQuote(id: number) {
+		const token =this.authService.getToken();
+		
+		return this.http.delete('http://localhost/LAV/public/api/quote/' + id+'?token='+token);
+	}
+}
